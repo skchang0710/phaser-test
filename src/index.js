9b1d76f2cc2c591749205c3c474cd727f940e9dc
Quincy Chang <quincy0710@hotmail.com>
2020-11-18 14:05:47 +0800

    make sample of isometric

--- src/index.js
+++ src/index.js
@@ -1,32 +1,59 @@
 import Phaser from "phaser";
-import logoImg from "./assets/logo.png";
+import IsoPlugin, { IsoPhysics } from "phaser3-plugin-isometric";
+import grassTileData from "./assets/grassTile.json";
+import grassTileImg from "./assets/grassTile.png";
+
+class playGame extends Phaser.Scene {
+  constructor() {
+    const sceneConfig = {
+      key: "IsoCollisionExample",
+      mapAdd: { isoPlugin: "iso", isoPhysics: "isoPhysics" }
+    };
+    super(sceneConfig);
+  }
+  preload() {
+		this.load.scenePlugin({
+			key: "IsoPlugin",
+			url: IsoPlugin,
+			sceneKey: "iso"
+		});
+	
+		this.load.scenePlugin({
+			key: "IsoPhysics",
+			url: IsoPhysics,
+			sceneKey: "isoPhysics"
+		});
+
+		this.load.spritesheet("grassTile", grassTileImg, {
+			frameWidth: grassTileData.width, // from json
+			frameHeight: grassTileData.height, // from json
+			startFrame: 0 // only using this frame, this could be a this.load.image
+		});
+  }
+  create() {
+		// create tile group
+		this.isoTiles = this.add.group();
+		// set default camera placement
+		this.iso.projector.origin.setTo(0.5, 0.3);
+
+		let tile = this.add.isoSprite(
+			0,
+			0,
+			-10,
+			"grassTile",
+			this.isoTiles
+		);
+		tile.setInteractive();
+  }
+}
 
 const config = {
   type: Phaser.AUTO,
   parent: "phaser-example",
   width: 800,
   height: 600,
-  scene: {
-    preload: preload,
-    create: create
-  }
+  scene: playGame
 };
 
 const game = new Phaser.Game(config);
 
-function preload() {
-  this.load.image("logo", logoImg);
-}
-
-function create() {
-  const logo = this.add.image(400, 150, "logo");
-
-  this.tweens.add({
-    targets: logo,
-    y: 450,
-    duration: 2000,
-    ease: "Power2",
-    yoyo: true,
-    loop: -1
-  });
-}
